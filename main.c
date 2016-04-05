a83e2b2850ab6133c6428dfbd38e3032fe8e2bc9
georgi <georgi@georgi-Aspire-E5-572G>
2016-04-05 18:38:22 +0300

    Chapter 9, Program 9.1 Program to illustrate a structure

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
  * File:   main.c
  * Author: georgi
  *
- * Created on April 5, 2016, 6:35 PM
+ * Created on April 5, 2016, 1:44 PM
  */
 
 #include <stdio.h>
@@ -17,7 +17,23 @@
 /*
  * 
  */
+
+// Program to illustrate a structure
+
 int main(int argc, char** argv) {
+    struct date {
+        int month;
+        int day;
+        int year;
+    };
+    
+    struct date today;
+    
+    today.month = 9;
+    today.day = 25;
+    today.year = 2004;
+    
+    printf("Today's date is %i/%i/%.2i", today.month, today.day, today.year % 100);
 
     return (EXIT_SUCCESS);
 }
