2141d2098b21979e2e6ffac59ea0b5e54e72c8c3
georgi <georgi@georgi-Aspire-E5-572G>
2016-04-05 18:39:23 +0300

    Chapter 9, Program 9.2 Program to determine tomorow's date

--- main.c
+++ main.c
@@ -8,17 +8,17 @@
  * File:   main.c
  * Author: georgi
  *
- * Created on April 5, 2016, 1:44 PM
+ * Created on April 5, 2016, 2:02 PM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 /*
- * 
+ * Program 9.2
  */
 
-// Program to illustrate a structure
+// Program to determine tomorrow's date
 
 int main(int argc, char** argv) {
     struct date {
@@ -27,13 +27,29 @@ int main(int argc, char** argv) {
         int year;
     };
     
-    struct date today;
+    struct date today, tomorow;
     
-    today.month = 9;
-    today.day = 25;
-    today.year = 2004;
+    const int daysPerMonths[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     
-    printf("Today's date is %i/%i/%.2i", today.month, today.day, today.year % 100);
+    printf("Enter today's date (mm dd yyyy: ");
+    scanf("%i%i%i", &today.month, &today.day, &today.year);
+    
+    if (today.day != daysPerMonths[today.month - 1]) {
+        tomorow.day = today.day + 1;
+        tomorow.month = today.month;
+        tomorow.year = today.year;
+    } else if (today.month == 12) {      // end of year
+        tomorow.day = 1;
+        tomorow.month = 1;
+        tomorow.year = today.year + 1;
+    }
+    else {
+        tomorow.day = 1;
+        tomorow.month = today.month + 1;
+        tomorow.year = today.year;
+    }
+    
+    printf("Tomorow's date is %i/%i/%.2i\n", tomorow.month, tomorow.day, tomorow.year%100);
 
     return (EXIT_SUCCESS);
 }
