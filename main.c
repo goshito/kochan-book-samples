8dae247832bafec0a3686e7321109814b85f0075
georgi <georgi@georgi-Aspire-E5-572G>
2016-04-06 00:35:54 +0300

    Chapter 9 Program 9.5 Updating Time by One Second

--- main.c
+++ main.c
@@ -8,87 +8,57 @@
  * File:   main.c
  * Author: georgi
  *
- * Created on April 5, 2016, 7:42 PM
+ * Created on April 6, 2016, 12:12 AM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-/* Chapter 9
- *  Program 9.4 Revising the Program to Determine Tomorrow's Date, Version 2
+/*
+ * Chapter 9, Program 9.5 Updating Time by One Second
  */
 
-// Program to determine tomorrow's date
+// Program to update the time by one second
 
-struct date {
-    int month;
-    int day;
-    int year;
+struct time {
+    int hour;
+    int minutes;
+    int seconds;
 };
 
-// Function to calculate tomorrow's date
-
-struct date dateUpdate(struct date today) {
-    struct date tomorrow;
-    int numberOfDays(struct date d);
-    
-    if (today.day != numberOfDays(today)) {
-        tomorrow.day = today.day + 1;
-        tomorrow.month = today.month;
-        tomorrow.year = today.year;
-    } else if (today.month == 12) {     // end of year
-        tomorrow.day = 1;
-        tomorrow.month = 1;
-        tomorrow.year = today.year + 1;
-    } else {                            // end of month
-        tomorrow.day = 1;
-        tomorrow.month = today.month + 1;
-        tomorrow.year = today.year;
-    }
-    
-    return tomorrow;
-}
-
-// Function to find the number of days in a month
-
-int numberOfDays(struct date d) {
-    int days;
-    bool isLeapYear(struct date d);
-    const int daysPerMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    
-    if (isLeapYear && d.month == 2)
-        days = 29;
-    else
-        days = daysPerMonth[d.month - 1];
-    
-    return days;
-}
-
-// Function to determine of it's a leap year
-
-bool isLeapYear(struct date d) {
-    bool leapYearFlag;
-    
-    if ((d.year % 4 == 0 && d.year % 100 != 0) || d.year % 400 == 0 )
-        leapYearFlag = true;    // It's a leap year
-    else
-        leapYearFlag = false;   // Not a leap year
-    
-    return leapYearFlag;
-}
-
 int main(int argc, char** argv) {
-    struct date dateUpdate(struct date today);
-    struct date thisDay, nextDay;
+    struct time timeUpdate(struct time now);
+    struct time currentTime, nextTime;
     
-    printf("Enter today's date (mm dd yyyy): ");
-    scanf("%i%i%i", &thisDay.month, &thisDay.day, &thisDay.year);
+    printf("Enter the time (hh:mm:ss): ");
+    scanf("%i:%i:%i", &currentTime.hour, &currentTime.minutes, &currentTime.seconds);
     
-    nextDay = dateUpdate(thisDay);
+    nextTime = timeUpdate(currentTime);
     
-    printf("Tomorrow's date is %i/%i/%.2i.\n", nextDay.month, nextDay.day, nextDay.year % 100);
+    printf("Updated time is %.2i.%.2i.%.2i\n", nextTime.hour, nextTime.minutes, nextTime.seconds);
 
     return (EXIT_SUCCESS);
+}
+
+// Function to update time by one second
+
+struct time timeUpdate(struct time now) {
+    ++now.seconds;
+    
+    if (now.seconds == 60) {        // next minute
+        now.seconds = 0;
+        ++now.minutes;
+        
+        if (now.minutes == 60) {    // next hour
+            now.minutes = 0;
+            ++now.hour;
+            
+            if (now.hour == 24) {   // midnight
+                now.hour = 0;
+            }
+        }
+    }
+    
+    return now;
 }
 
